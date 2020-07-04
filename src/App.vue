bfe41cd0cbee96d2a895f23c19c3e668a368b3e3
kuluru96 <muralidhark36@gmail.com>
2020-07-05 00:31:12 +0200

    toolbar complete

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,66 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+    <VNavigationDrawer permanent app  width="300">
+      <v-row cols="12" justify="center" class="primary">
+          <h1 height="100" class="white--text py-5" >Student e-portal</h1>
+      </v-row>
+        <v-container grid-list-xs>
+            <v-col height="fill-height">
+                <v-row cols="12" justify="center">
+                    <img class="profile elevation-12" src="https://pbs.twimg.com/profile_images/1157994852791050240/wLGN40sL_400x400.jpg" alt="" height="100">
+                </v-row>
+                <v-row cols="12" justify="center" class="text-sm-center py-2">
+                    <h2 class="background--text">{{sName}}</h2>
+                </v-row>
+                <v-row v-for="item in panes" :key="item.id" cols="12" justify="left" class="text-sm-center py-2" flat>
+                      <VBtn text width="100%" ><v-icon small left>{{item.icon}}</v-icon> {{ item.name }}</VBtn>
+                </v-row>
+            </v-col>
+        </v-container>
+    </VNavigationDrawer>
 
     <v-content>
-      <HelloWorld/>
+      <router-view class="px-5 pt-2"></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
-
+  
   components: {
-    HelloWorld,
   },
 
   data: () => ({
-    //
+    sName: 'Pruthvi Vikram',
+    panes: [
+      {"name": "Profile", "icon": "mdi-account"},
+      {"name": "Grades", "icon": "mdi-account"},
+      {"name": "Courses", "icon": "mdi-account"},
+      {"name": "Discussion forum", "icon": "mdi-account"},
+      {"name": "Logout", "icon": "mdi-account"}
+    ]
   }),
 };
 </script>
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Yellowtail&display=swap');
+
+  /* Hide scrollbar for Chrome, Safari and Opera */
+  body::-webkit-scrollbar {
+    display: none;
+  }
+
+  /* Hide scrollbar for IE, Edge and Firefox */
+  body {
+    -ms-overflow-style: none;  /* IE and Edge */
+    scrollbar-width: none;  /* Firefox */
+  }
+
+  img.profile{
+    border-radius: 100px;
+  }
+
+</style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,27 +4,14 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    icons: {
+        iconfont: 'mdi', // default - only for display purposes
+      },
     theme: {
         themes: {
             light: {
-                background: '#000000',
-                primary: '#114B5F',
-                secondary: '#ff5722',
-                accent: '#9c27b0',
-                error: '#e91e63',
-                warning: '#ffc107',
-                info: '#2196f3',
-                success: '#4caf50'
-            },
-            dark: {
-                background: '#FFFFFF',
-                primary: '#114B5F',
-                secondary: '#ff5722',
-                accent: '#9c27b0',
-                error: '#e91e63',
-                warning: '#ffc107',
-                info: '#2196f3',
-                success: '#4caf50'
+                primary : '004c4c',
+                heading: 'white'
             }
         }
     }
